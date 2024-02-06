44221f03eb409688a94f5bbb0771612a29fc42c6
Gabriel Nascimento <gabriielns1@gmail.com>
2024-02-06 20:51:12 -0300

    primeiro commit

---
+++ JS/aula02.js
@@ -0,0 +1,69 @@
+/*
+   arquivo JS externo
+*/
+
+
+document.write("Olá Pessoal!")
+console.log("Denovo Olá Pessoal!")
+//alert("Eaii Pessoal")
+
+var mensagem = "" // não é legal usar
+let msg = "Minha mensagem" // no lugar use isso
+let msg1 = 'Outra mensagem'
+let meuPeso = 85.6
+let minhaAltura = 1.82
+let minhaIdade = 18
+let ehDoador = false
+let teste = null
+
+
+// Criar CONSTANTES de VARIAVEIS e CONSTANTES
+
+const PI = 3.1415
+const TAXA = 0.5
+
+// ESCREVER O CONTEUDO DE VARIAVEIS E CONSTANTES
+
+document.write(msg)
+document.write(msg1)
+document.write(meuPeso)
+document.write(minhaAltura)
+document.write(minhaIdade)
+document.write(ehDoador)
+document.write(teste)
+document.write(PI)
+document.write(TAXA)
+
+// ESCREVER O CONTEUDO DE VARIAVEIS e CONSTANTES 
+
+document.write("<p> Mensagem: " + msg + "</p>")
+
+
+document.write("<p> Mensagem2: " + msg1 + "</p>")
+
+
+document.write("<p> Peso: " + meuPeso + " g </p>")
+
+
+document.write("<p> Altura: " + minhaAltura + "m </p>")
+
+
+document.write("<p> Idade: " + minhaIdade + " idade </p>")
+
+
+document.write("<p> Doador? " + ehDoador + "</p>")
+
+
+document.write("<p> Teste: " + teste + "</p>")
+
+
+document.write("<p> PI " + PI + "</p>")
+
+
+document.write("<p> TAXA " + TAXA + "</p>")
+
+
+
+ 
+
+
---
+++ aula03.js
@@ -0,0 +1,40 @@
+let n1 = 10 //number inteiro
+let n2 = 2 //number inteiro
+let n3 = "2" //string texto
+
+document.write('N1 ' + n1 + '<br>')
+document.write('N2 ' + n2 + '<br>')
+document.write('N3 ' + n3 + '<br>')
+
+
+console.log(n1 + n2) //adição
+console.log(n1 - n2) //subtração
+console.log(n1 * n2) //multiplicação
+console.log(n1 / n2) //divisao
+console.log(n1 % n2) //resto ou modulo
+
+
+console.log(n1 > n2)
+console.log(n1 < n2)
+console.log(n1 >= n2)
+console.log(n1 <= n2)
+   
+   
+console.log(n1 == n2) //igual a 
+console.log(n1 === n2) //identico
+console.log(n1 != n2) //diferente de não =
+console.log(n1 !== n2) //Nao e identico
+
+
+let a = true
+let b = false
+console.log(a && b) //E AND
+console.log(a || b) //OU OR
+console.log(!a) //NAO NOT negacao
+console.log(!b)
+
+
+
+
+
+
